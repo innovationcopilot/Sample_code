9e881f3334d186b10b07ea0446ca17e9573af55d
innovationcopilot <137910577+innovationcopilot@users.noreply.github.com>
2023-09-13 20:27:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
+# Importing necessary libraries and modules
+import openai
 import streamlit as st
+import os
+import time
+
+# Importing custom modules for setup and functionalities
 from setup_st import set_design, initialize_session_state, sidebar, clear_button, download_button, get_user_config
-from helper_functions import generate_response
+from helper_functions import generate_response, report_call, generate_validation_report  # Assuming these functions exist in your helper_functions.py
 from index_functions import construct_index
 
-# Initialize session state
+# Setting up environment variables for OpenAI API key
+os.environ["OPENAI_API_KEY"] = 'your-openai-api-key-here'
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize session state variables if they don't exist
 initialize_session_state()
 
-# Initialize UI/UX
+# Setup Streamlit UI/UX elements
 set_design()
 sidebar()
+clear_button()
+download_button()
 get_user_config()
 
-# Initialize knowledge base index
-directory_path = "/path/to/your/documents"  # Make sure this path exists
+# Initialize the knowledge base index
+directory_path = "/path/to/your/documents"  # Replace with your actual directory path
 index = construct_index(directory_path)
 
-# Main chatbot logic
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
+# Initialize progress bar and set to 0
+progress_bar = st.progress(0)
 
+# Main chat loop to display messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+# Accept user input
+if prompt := st.chat_input("How would you like to reply?"):
+    
+    # Add user's message to the chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     
-    # Generate chatbot response
-    for response in generate_response(
-            prompt, st.session_state.messages, index,
-            st.session_state['model_name'], st.session_state['temperature'],
-            st.session_state['current_stage_index']):
-        st.session_state.messages.append({"role": "assistant", "content": response['content']})
-    else:
-        st.warning("Not enough conversation history.")
+    # Call generate_response function to get chatbot's reply
+    # This function is assumed to be defined in your helper_functions.py
+    for generated in generate_response("system prompt here", prompt, st.session_state.messages, st.session_state['model_name'], st.session_state['temperature'], "stage name here", index):
+        chatbot_reply = generated["content"]
 
-    clear_button()
-    download_button()
+    # Display chatbot's reply
+    with st.chat_message("assistant"):
+        st.markdown(chatbot_reply)
+    
+    # Add chatbot's reply to chat history
+    st.session_state.messages.append({"role": "assistant", "content": chatbot_reply})
+    
+    # Update the progress bar
+    # Here, you can implement your logic to update the progress_bar
+    # For the sake of this example, I'm incrementing it by 10% each time
+    current_progress = progress_bar.progress + 0.1
+    progress_bar.progress(current_progress)
