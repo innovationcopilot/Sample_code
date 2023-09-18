58db4726952282083db24d3885dd6e6eeb63ef74
innovationcopilot <137910577+innovationcopilot@users.noreply.github.com>
2023-09-18 15:35:49 -0500

    Update index_functions.py

--- index_functions.py
+++ index_functions.py
@@ -15,56 +15,10 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 from llama_index import SimpleDirectoryReader
 
-
-# Function for constructing an index out of a knowledge base and appending indexed information to our prompt
-'''def construct_index(directory_path):
-    if os.path.exists('index.json'):
-        index = GPTSimpleVectorIndex.load_from_disk('index.json')
-        return index
-
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=.3, model_name="gpt-3.5-turbo-16k-0613", max_tokens=num_outputs))
-
-    documents = SimpleDirectoryReader(directory_path).load_data()
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    index.save_to_disk('index.json')
-    return index
-    '''
-'''
-# Function for constructing an index out of a knowledge base and appending indexed information to our prompt
-def construct_index(directory_path):
-    
-    if directory_path is not None:
-        # Read the uploaded file
-        uploaded_text = directory_path.read().decode('utf-8')
-
-        # Tokenize or otherwise preprocess the uploaded_text as necessary
-        # In your existing code, you could replace this with whatever SimpleDirectoryReader().load_data() does
-        documents = uploaded_text.split('\n')  # For example, if each line of the file is a separate "document"
-    else:
-        return None  # No file uploaded
-
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=.3, model_name="gpt-3.5-turbo-16k-0613", max_tokens=num_outputs))
-
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    return index
-'''
+# Function to load in data found in the 'data' folder of the central repository; To upload your own data, simply remove the existing data in that folder and upload your own. Don't forget to update the prompt below!
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing the data – hang tight! This shouldn't take more than a minute."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Innovation CoPilot and your job is to answer questions about it. Assume that all questions are related to the Innovation CoPilot. Keep your answers technical and based on facts – do not hallucinate features."))
@@ -106,6 +60,24 @@ def get_relevant_articles(keywords, articles, num_articles=2):
     most_similar_articles_indices = cosine_similarities.argsort()[:-num_articles - 1:-1]
     most_similar_articles = [article_ids[i] for i in most_similar_articles_indices]
 
-
-
     return {article: articles[article] for article in most_similar_articles}
+
+# Function for constructing an index out of a knowledge base and appending indexed information to our prompt (another implementation that can be run locally rather than through github)
+def construct_index(directory_path):
+    if os.path.exists('index.json'):
+        index = GPTSimpleVectorIndex.load_from_disk('index.json')
+        return index
+
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=.3, model_name="gpt-3.5-turbo-16k-0613", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index.save_to_disk('index.json')
+    return index
