acbeee03ffd3b7b00a07558456155229065c2f7d
innovationcopilot <137910577+innovationcopilot@users.noreply.github.com>
2023-09-20 11:34:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from setup_st import *
 from helper_functions import *
 from index_functions import *
 
+# Initialize session state variables if they don't exist
+initialize_session_state()
+
 # Setup Streamlit UI/UX elements
 set_design()
 sidebar()
 get_user_config()
 clear_button()
 download_button()
-
-# Initialize session state variables if they don't exist
-initialize_session_state()
 
 # Setting up environment variables for OpenAI API key
 if 'api_key' in st.session_state and st.session_state['api_key']:
